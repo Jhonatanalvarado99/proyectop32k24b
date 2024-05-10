a116724e45058342e779e9553da31d29c996bfc5
Melany Secaida <melanysecaidaa@gmail.com>
2024-05-09 23:51:03 -0600

    agregando ayuda de ciclos

--- scripts/Proyecto_Final_Universitario.sql
+++ scripts/Proyecto_Final_Universitario.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS usuario (
   id_usuario INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(60) NOT NULL,
   password VARCHAR(25)NOT NULL,
-  PRIMARY KEY (id_usuario) )
+  PRIMARY KEY (id_usuario) )id_usuario
 ENGINE = InnoDB CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `educativo`.`Alumnos`
